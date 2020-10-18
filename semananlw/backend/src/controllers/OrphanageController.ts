7cbd0ac5c1136c6424b6ffb1554d7d56c39c7ac8
BolzBit <mateus_desktop@outlook.com>
2020-10-18 12:56:55 -0300

    primeiro commit

---
+++ semananlw/backend/src/controllers/OrphanageController.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+
+import orphanages_view from '../views/orphanages_view';
+import Orphanage from "../models/Orphanage";
+
+export default {
+
+    async index(request: Request, response: Response){
+        const orphanageRepository = getRepository(Orphanage);
+
+        const orphanages = await orphanageRepository.find({
+            relations: ['images']
+        });
+
+        return response.json(orphanages_view.renderMany(orphanages));
+    },
+
+    async show(request: Request, response: Response){
+        const { id } = request.params;
+
+        const orphanageRepository = getRepository(Orphanage);
+
+        const orphanage = await orphanageRepository.findOneOrFail(id, {
+            relations: ['images']
+        });
+
+        return response.json(orphanages_view.render(orphanage));
+
+    },
+
+    async create(request: Request, response: Response){
+        console.log(request.files);
+
+        const {
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends
+        } = request.body;
+
+        const orphanageRepository = getRepository(Orphanage);
+
+        const requestImages = request.files as Express.Multer.File[];
+        const images = requestImages.map(image => {
+            return { path: image.filename } 
+        });
+
+        const orphanage = orphanageRepository.create({
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends,
+            images
+        });
+
+        await orphanageRepository.save(orphanage);
+
+        return response.status(201).json(orphanage);
+    }
+};
