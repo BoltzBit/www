219ae723e7a65e9174b84688e4ea518a4c659e6c
BolzBit <mateus_desktop@outlook.com>
2020-10-19 09:52:03 -0300

    alteração front

--- semananlw/web/src/pages/OrphanagesMap.tsx
+++ semananlw/web/src/pages/OrphanagesMap.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { FiPlus } from 'react-icons/fi'
+import { FiPlus, FiArrowRight } from 'react-icons/fi'
 import { Link } from 'react-router-dom';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/Local.svg';
 import '../styles/pages/OrphanagesMap.css';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+    iconSize: [58, 68],
+    iconAncor: [29,68],
+    popupAnchor: [170, 10]
+});
 
 function OrphanagesMap(){
     return(
@@ -31,6 +39,18 @@ function OrphanagesMap(){
                 style={{ width: '100%', height: '100%'}}
             >
                 <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+                <Marker
+                    icon={mapIcon}
+                    position={[-23.321264, -51.2358034]}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        Lar Feliz
+                        <Link to="">
+                            <FiArrowRight />
+                        </Link>
+                    </Popup>
+                </Marker>
             </Map>
 
             <Link to="/" className="create-orphanage">
